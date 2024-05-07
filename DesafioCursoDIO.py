863d188781a55516fb26871e3d7cc6dcdf698775
Hugo Deleon <163484018+HDeleondev@users.noreply.github.com>
2024-05-07 17:24:08 -0300

    Add files via upload

---
+++ DesafioCursoDIO.py
@@ -0,0 +1,17 @@
+consumo = float(input())
+
+
+def recomendar_plano():
+  consumo.append (int())
+  
+if  consumo <= 10:
+    print("Plano Essencial Fibra - 50Mbps")
+elif  consumo <= 20:
+    print("Plano Prata Fibra - 100Mbps")
+else:
+    print("Plano Premium Fibra - 300Mbps")
+
+
+
+
+
